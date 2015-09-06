00a9ffff57e305e2f61b73ebcf41999e02d70296
Andrew Broman <broman@acm.org>
2015-09-05 21:50:57 -0500

    Initial commit

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,18 @@
+import Greett from './greet';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Greet />
+    );
+  }
+}
+
+let documentReady = () => {
+  React.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
